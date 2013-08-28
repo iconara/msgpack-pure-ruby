46a5fbd22b1ef1815e300a213433b06b64c8be6f
Theo <theo@iconara.net>
2013-08-28 08:11:31 +0200

    Simplify the decoding with some OO

--- lib/msgpack.rb
+++ lib/msgpack.rb
@@ -6,123 +6,152 @@ module MessagePack
   end
 
   def self.unpack(bytes)
-    decode(bytes, 0).first
+    Decoder.new(bytes).next
   end
 
-  module Decoding
+  class Decoder
+    def initialize(bytes)
+      @bytes = bytes
+      @offset = 0
+    end
+
+    def next
+      consume_next
+    end
+
+    private
+
     FLOAT_FMT = 'g'.freeze
     DOUBLE_FMT = 'G'.freeze
 
-    def decode_int16(bytes, offset)
-      (bytes.getbyte(offset) << 8) | bytes.getbyte(offset + 1)
+    def consume_byte
+      b = @bytes.getbyte(@offset)
+      @offset += 1
+      b
     end
 
-    def decode_int32(bytes, offset)
-      (bytes.getbyte(offset) << 24) | (bytes.getbyte(offset + 1) << 16) | (bytes.getbyte(offset + 2) << 8) | bytes.getbyte(offset + 3)
+    def consume_int16
+      (consume_byte << 8) | consume_byte
     end
 
-    def decode_int64(bytes, offset)
-      x  = (bytes.getbyte(offset + 0) << 56)
-      x |= (bytes.getbyte(offset + 1) << 48)
-      x |= (bytes.getbyte(offset + 2) << 40)
-      x |= (bytes.getbyte(offset + 3) << 32)
-      x |= (bytes.getbyte(offset + 4) << 24)
-      x |= (bytes.getbyte(offset + 5) << 16)
-      x |= (bytes.getbyte(offset + 6) << 8)
-      x |=  bytes.getbyte(offset + 7)
-      x
+    def consume_int32
+      (consume_byte << 24) | (consume_byte << 16) | (consume_byte << 8) | consume_byte
     end
 
-    def decode_array(bytes, offset, size, bytes_consumed)
-      array = Array.new(size)
-      size.times do |i|
-        e, bc = decode(bytes, offset + bytes_consumed)
-        array[i] = e
-        bytes_consumed += bc
-      end
-      return array, bytes_consumed
+    def consume_int64
+      n  = (consume_byte << 56)
+      n |= (consume_byte << 48)
+      n |= (consume_byte << 40)
+      n |= (consume_byte << 32)
+      n |= (consume_byte << 24)
+      n |= (consume_byte << 16)
+      n |= (consume_byte << 8)
+      n |=  consume_byte
+      n
     end
 
-    def decode(bytes, offset)
-      b = bytes.getbyte(offset)
+    def consume_float
+      f = @bytes[@offset, 4].unpack(FLOAT_FMT).first
+      @offset += 4
+      f
+    end
+
+    def consume_double
+      d = @bytes[@offset, 8].unpack(DOUBLE_FMT).first
+      @offset += 8
+      d
+    end
+
+    def consume_string(size)
+      s = @bytes[@offset, size]
+      s.force_encoding(Encoding::UTF_8)
+      @offset += size
+      s
+    end
+
+    def consume_binary(size)
+      s = @bytes[@offset, size]
+      s.force_encoding(Encoding::BINARY)
+      @offset += size
+      s
+    end
+
+    def consume_array(size)
+      Array.new(size) { consume_next }
+    end
+
+    def consume_next
+      b = consume_byte
       if b == 0xc0
-        return nil, 1
+        nil
       elsif b == 0xc3
-        return true, 1
+        true
       elsif b == 0xc2
-        return false, 1
+        false
       elsif b <= 0b01111111
-        return b, 1
+        b
       elsif b & 0b11100000 == 0b11100000
-        return b - 0x100, 1
+        b - 0x100
       elsif b == 0xcc
-        return bytes.getbyte(offset + 1), 2
+        consume_byte
       elsif b == 0xd0
-        return bytes.getbyte(offset + 1) - 0x100, 2
+        consume_byte - 0x100
       elsif b == 0xcd
-        return decode_int16(bytes, offset + 1), 3
+        consume_int16
       elsif b == 0xd1
-        return (decode_int16(bytes, offset + 1)) - 0x10000, 3
+        consume_int16 - 0x10000
       elsif b == 0xce
-        return decode_int32(bytes, offset + 1), 5
+        consume_int32
       elsif b == 0xd2
-        return decode_int32(bytes, offset + 1) - 0x100000000, 5
+        consume_int32 - 0x100000000
       elsif b == 0xcf
-        return decode_int64(bytes, offset + 1), 9
+        consume_int64
       elsif b == 0xd3
-        return decode_int64(bytes, offset + 1) - 0x10000000000000000, 9
+        consume_int64 - 0x10000000000000000
       elsif b == 0xca
-        return bytes[offset + 1, 4].unpack(FLOAT_FMT).first, 5
+        consume_float
       elsif b == 0xcb
-        return bytes[offset + 1, 8].unpack(DOUBLE_FMT).first, 9
+        consume_double
       elsif b & 0b11100000 == 0b10100000
         size = b & 0b00011111
-        return bytes[offset + 1, size].force_encoding(Encoding::UTF_8), size + 1
+        consume_string(size)
       elsif b == 0xd9
-        size = bytes.getbyte(offset + 1)
-        return bytes[offset + 2, size].force_encoding(Encoding::UTF_8), size + 2
+        size = consume_byte
+        consume_string(size)
       elsif b == 0xc4
-        size = bytes.getbyte(offset + 1)
-        return bytes[offset + 2, size].force_encoding(Encoding::BINARY), size + 2
+        size = consume_byte
+        consume_binary(size)
       elsif b == 0xda
-        size = decode_int16(bytes, offset + 1)
-        return bytes[offset + 3, size].force_encoding(Encoding::UTF_8), size + 3
+        size = consume_int16
+        consume_string(size)
       elsif b == 0xc5
-        size = decode_int16(bytes, offset + 1)
-        return bytes[offset + 3, size].force_encoding(Encoding::BINARY), size + 3
+        size = consume_int16
+        consume_binary(size)
       elsif b == 0xdb
-        size = decode_int32(bytes, offset + 1)
-        return bytes[offset + 5, size].force_encoding(Encoding::UTF_8), size + 5
+        size = consume_int32
+        consume_string(size)
       elsif b == 0xc6
-        size = decode_int32(bytes, offset + 1)
-        return bytes[offset + 5, size].force_encoding(Encoding::BINARY), size + 5
+        size = consume_int32
+        consume_binary(size)
       elsif b & 0b11110000 == 0b10010000
         size = b & 0b00001111
-        return decode_array(bytes, offset, size, 1)
+        consume_array(size)
       elsif b == 0xdc
-        size = decode_int16(bytes, offset + 1)
-        return decode_array(bytes, offset + 2, size, 1)
+        size = consume_int16
+        consume_array(size)
       elsif b == 0xdd
-        size = decode_int32(bytes, offset + 1)
-        return decode_array(bytes, offset + 4, size, 1)
+        size = consume_int32
+        consume_array(size)
       elsif b & 0b11110000 == 0b10000000
         size = b & 0b00001111
-        r = decode_array(bytes, offset, size * 2, 1)
-        r[0] = Hash[*r[0]]
-        return r
+        Hash[*consume_array(size * 2)]
       elsif b == 0xde
-        size = decode_int16(bytes, offset + 1)
-        r = decode_array(bytes, offset + 2, size * 2, 1)
-        r[0] = Hash[*r[0]]
-        return r
+        size = consume_int16
+        Hash[*consume_array(size * 2)]
       elsif b == 0xdf
-        size = decode_int32(bytes, offset + 1)
-        r = decode_array(bytes, offset + 4, size * 2, 1)
-        r[0] = Hash[*r[0]]
-        return r
+        size = consume_int32
+        Hash[*consume_array(size * 2)]
       end
     end
   end
-
-  extend Decoding
 end
